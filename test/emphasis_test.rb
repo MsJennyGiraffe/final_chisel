f414622a9acfa1a00094bc84289910e0783afef4
Jennifer <jennifersoden@hotmail.com>
2016-03-24 16:09:37 -0600

    emphasis

--- test/emphasis_test.rb
+++ test/emphasis_test.rb
@@ -1,3 +1,5 @@
+#2 runs, 2 assertions, 0 failures, 0 errors, 0 skips
+
 require 'minitest/autorun'
 require_relative '../lib/emphasis'
 
@@ -5,3 +7,13 @@ class EmphasisTest < Minitest::Test
   def setup
     @emphasis = Emphasis.new
   end
+
+  def test_will_replace_singular_asterics_with_emphasis_tags
+    assert_equal "So much <em>emphasis</em>", @emphasis.make_emphasis("So much *emphasis*")
+  end
+
+  def test_can_replace_multiples_at_a_time
+    assert_equal "Even <em>more</em> <em>emphasis</em>", @emphasis.make_emphasis("Even *more* *emphasis*")
+  end
+
+end
