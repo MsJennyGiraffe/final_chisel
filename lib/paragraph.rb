6f3531fc6537938ba03ca08ebe5a72cd1905bb02
Jennifer <jennifersoden@hotmail.com>
2016-03-24 18:04:24 -0600

    paragraphs

--- lib/paragraph.rb
+++ lib/paragraph.rb
@@ -4,3 +4,18 @@
 
 #information needed:
   #contents from markdown file
+
+class Paragraph
+
+  def format_paragraphs(line)
+    if line.start_with?("<em>")
+      "<p>#{line}</p>"
+    elsif line.start_with?("<strong>")
+      "<p>#{line}</p>"
+    elsif !line.start_with?("<")
+      "<p>#{line}</p>"
+    else
+      line
+    end
+  end
+end
